87c15a6e9dd82a7e0954cdcb60412fe013f8add6
DharaneedaranKS <ksd.ynk@gmail.com>
2023-11-30 21:50:09 +0530

    final_code

--- MoveIt/mycobot_grasping/src/grasp_demo.py
+++ MoveIt/mycobot_grasping/src/grasp_demo.py
@@ -6,13 +6,16 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import math 
 
 def start_up() :
   move_group.set_named_target("start_up")
   plan_start_up = move_group.plan()
-  move_group.go(wait = True)
+  plan = move_group.go(wait = True)
+  # move_group.execute(plan)
+  return plan
 
-def pick_up() :
+def drop_off() :
   group_variable_values = move_group.get_current_joint_values()
 
   group_variable_values = [1, 1.5, 0, -1.5, -1.5, 0]
@@ -20,22 +23,47 @@ def pick_up() :
   move_group.set_joint_value_target(group_variable_values)
 
   plan1 = move_group.plan()
-  plan2 = move_group.go(wait = True)
+  plan = move_group.go(wait = True)
+  return plan
+  # move_group.execute(plan2)
 
-def gripper_close() :
+"""def gripper_close() :
   hand_variable_values = hand_group.get_current_joint_values()
-  #print(hand_variable_values)
-  hand_variable_values = [-0.166, 0.166, 0.166, -0.166, -0.166, 0.166]
-  hand_group.set_joint_value_target(hand_variable_values)
-  planx = hand_group.plan()
-  planx2 = hand_group.go(wait = True)
+  print(hand_variable_values)
+  hand_variable_values[0] = -0.21
+  #hand_group.set_joint_value_target("gripper_controller", hand_variable_values)
+  #planx = hand_group.plan()
+  hand_group.go(hand_variable_values, wait = True)
+  hand_group.stop()"""
 
-def pick_by_position():
-  move_group.set_position_target([0.2, 0.2, 0.1], "joint6")
+ 
 
 def reach_named_position():
     move_group.set_named_target("pick_up_extended")
-    return move_group.execute(move_group.plan()[1], wait=True)
+    plan_pick_up = move_group.plan()
+    plan = move_group.go(wait = True)
+    #move_group.execute(plan)
+    return plan
+
+def table() :
+  table_pose = geometry_msgs.msg.PoseStamped()
+  table_pose.header.frame_id = "g_base"
+  table_pose.pose.orientation.w = 1.0
+  table_pose.pose.position.x = 0.0
+  table_pose.pose.position.y = -0.3
+  table_pose.pose.position.z = 0.1  # above the frame
+  table_name = "table"
+  SCENE.add_box(table_name, table_pose, size=(0.1, 0.1, 0.20))
+
+def box() :
+  box_pose = geometry_msgs.msg.PoseStamped()
+  box_pose.header.frame_id = "g_base"
+  box_pose.pose.orientation.w = 1.0
+  box_pose.pose.position.x = -0.04
+  box_pose.pose.position.y = 0.29
+  box_pose.pose.position.z = 0.0  # above the frame
+  box_name = "box"
+  SCENE.add_box(box_name, box_pose, size=(0.02, 0.02, 0.02))
 
 
 if __name__ == '__main__':
@@ -48,32 +76,26 @@ if __name__ == '__main__':
   hand_name = "hand_mycobot"
   move_group = moveit_commander.MoveGroupCommander(group_name)
   hand_group = moveit_commander.MoveGroupCommander(hand_name)
+  eef_link = hand_group.get_end_effector_link()
+  touch_links = robot.get_link_names(group=hand_name)
 
   display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                 moveit_msgs.msg.DisplayTrajectory,
                                                 queue_size=20)
+  table()
+  box()
   
-  box_pose = geometry_msgs.msg.PoseStamped()
-  box_pose.header.frame_id = "g_base"
-  box_pose.pose.orientation.w = 1.0
-  box_pose.pose.position.x = 0.0
-  box_pose.pose.position.y = -0.3
-  box_pose.pose.position.z = 0.1  # above the frame
-  box_name = "box"
-  SCENE.add_box(box_name, box_pose, size=(0.1, 0.1, 0.20))
-  
-  start_up()
-  gripper_close()
-  #reach_named_position()
-
-  #pick_up()
-
   #start_up()
+  #gripper_close()
+  plan1 = reach_named_position()
+  
+  SCENE.attach_box(eef_link, "box", touch_links=touch_links)
+  plan2 = start_up()
 
-  #pick_by_position()
-
-  # start_up()
-
+  plan3 = drop_off()
+  SCENE.remove_attached_object(eef_link, name="box")
+  plan4 = start_up()
+  move_group.stop()
   moveit_commander.roscpp_shutdown()
 
 # move_group.execute(plan)
